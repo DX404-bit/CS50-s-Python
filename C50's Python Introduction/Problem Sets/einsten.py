f19b0cfd73414417f3b49809cf2fa8a86a29e7f1
Kevyn <diadiaqueiroz@gmail.com>
2025-02-23 16:23:48 -0300

    Add files via upload

---
+++ C50's Python Introduction/Problem Sets/einsten.py
@@ -0,0 +1,9 @@
+def main ():
+    m = int(input("m:"))
+    c = 300000000
+    e = int(c * c * m)
+    print(f"E:{e}")
+
+
+
+main()
---
+++ C50's Python Introduction/Problem Sets/indoor.py
@@ -0,0 +1,2 @@
+hello = input("What do you want to say? ")
+print(hello)
